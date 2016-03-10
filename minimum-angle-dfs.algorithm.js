ede183a6202e2a8702f3a34a3dccc7e434d1c245
gime <Giorgi.Megreli@betssongroup.com>
2016-03-10 09:52:31 +0200

    dfs implemented

--- minimum-angle-dfs.algorithm.js
+++ minimum-angle-dfs.algorithm.js
@@ -61,6 +61,60 @@ MinimumAngleDfsAlgorithm.prototype.start = function () {
 };
 
 MinimumAngleDfsAlgorithm.prototype.dfs = function (current, prev, dist) {
+    var me = this;
+    me.dist[current] = dist;
+    me.parent[current] = prev;
+    me.visited[current] = true;
+    if (current === me.tail)
+        return;
 
+    var i, adjAngles = [], adj;
+    for (i=0; i<me.adj[current].length; i++) {
+        adj = me.adj[current][i];
+        adjAngles.push({
+            adj: adj,
+            angle: MinimumAngleDfsAlgorithm.calculateAngle(
+                me.graph.vertices[current],
+                me.graph.vertices[adj],
+                me.graph.vertices[me.tail]
+            )
+        });
+    }
+
+    adjAngles.sort(function (a,b) {
+        return a.angle - b.angle;
+    });
+
+    for (i=0; i<adjAngles.length; i++) {
+        adj = adjAngles[i];
+        me.dfs(adj, current, dist+1);
+    }
+};
+
+/**
+ * calculates cos( angle(s,v,t) )
+ * @param s
+ * @param v
+ * @param t
+ */
+MinimumAngleDfsAlgorithm.calculateAngle = function (s, v, t) {
+    var dxsv = Math.abs(s.x - v.x);
+    var dysv = Math.abs(s.y - v.y);
+    var sv2 = dxsv * dxsv + dysv * dysv;
+
+    var dxvt = Math.abs(v.x - t.x);
+    var dyvt = Math.abs(v.y - t.y);
+    var vt2 = dxvt * dxvt + dyvt * dyvt;
+
+    var dxst = Math.abs(s.x - t.x);
+    var dyst = Math.abs(s.y - t.y);
+    var st2 = dxst * dxst + dyst * dyst;
+
+    /**
+     * st^2 = sv^2 + vt^2 - 2 sv vt cos(svt)
+     * cos(svt) = (sv^2 + vt^2 - st^2) / Math.sqrt( sv^2 * vt^2 )
+     */
+
+    return 1. * (sv2 + vt2 + st2) / Math.sqrt(sv2 * vt2);
 };
 
